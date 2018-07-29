f397553cd762ed0531dfba249e24f6075a163e28
Dan Siegel <me@dansiegel.net>
2018-07-28 19:49:45 -0700

    update artifact copy

--- build/Copy-Artifacts.ps1
+++ build/Copy-Artifacts.ps1
@@ -1,5 +1,6 @@
 $artifactDir = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath "Artifacts"
+$downloadDir = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath "Download"
 New-Item -ItemType Directory -Force -Path $artifactDir
 
-Get-ChildItem -Path $env:BUILD_ARTIFACTSTAGINGDIRECTORY | Where-Object { $_.Extension -match ".nupkg" } | Copy-Item -Destination $artifactDir
+Get-ChildItem -Path $downloadDir | Where-Object { $_.Extension -match ".nupkg" } | Copy-Item -Destination $artifactDir
 
