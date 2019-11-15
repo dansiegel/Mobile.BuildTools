518c9cb2ef2c8f12a4d7fb1ea77f313afd779aee
Dan Siegel <me@dansiegel.net>
2019-11-14 21:35:23 -0800

    use raw url

--- src/Mobile.BuildTools/Models/BuildToolsConfig.cs
+++ src/Mobile.BuildTools/Models/BuildToolsConfig.cs
@@ -7,7 +7,7 @@ namespace Mobile.BuildTools.Models
     public class BuildToolsConfig
     {
 #if !NETCOREAPP
-        private string _schema = "https://github.com/dansiegel/Mobile.BuildTools/blob/master/schema.json";
+        private string _schema = "https://raw.githubusercontent.com/dansiegel/Mobile.BuildTools/master/schema.json";
         [JsonProperty("$schema")]
         public string Schema
         {
