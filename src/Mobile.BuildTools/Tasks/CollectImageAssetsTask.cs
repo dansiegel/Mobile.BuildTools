7b94fdd478163b04f708318d425704ab50233118
Dan Siegel <me@dansiegel.net>
2019-11-15 09:13:24 -0800

    adding SearchPaths for Image Collection

--- src/Mobile.BuildTools/Tasks/CollectImageAssetsTask.cs
+++ src/Mobile.BuildTools/Tasks/CollectImageAssetsTask.cs
@@ -19,7 +19,7 @@ namespace Mobile.BuildTools.Tasks
 
         internal override void ExecuteInternal(IBuildConfiguration config)
         {
-            var generator = CreateGenerator(config.Platform);
+            var generator = CreateGenerator(config.Platform, config);
             if(generator is null)
             {
                 GeneratedImages = Array.Empty<ITaskItem>();
@@ -30,17 +30,62 @@ namespace Mobile.BuildTools.Tasks
             GeneratedImages = generator.Outputs.Select(x => x.ToTaskItem()).ToArray();
         }
 
-        private ImageCollectionGeneratorBase CreateGenerator(Platform platform)
+        private ImageCollectionGeneratorBase CreateGenerator(Platform platform, IBuildConfiguration config)
         {
             switch(platform)
             {
                 case Platform.Android:
-                    return new AndroidImageCollectionGenerator(this);
+                    return new AndroidImageCollectionGenerator(this)
+                    {
+                        SearchFolders = GetSearchPaths(config)
+                    };
                 case Platform.iOS:
                 case Platform.macOS:
-                    return new AppleImageCollectionGenerator(this);
+                    return new AppleImageCollectionGenerator(this)
+                    {
+                        SearchFolders = GetSearchPaths(config)
+                    };
                 default: return null;
             }
+        }
+
+        private IEnumerable<string> GetSearchPaths(IBuildConfiguration config)
+        {
+            var searchPaths = new List<string>();
+            var imageConfig = config.Configuration.Images;
+            if(imageConfig.Directories?.Any() ?? false)
+            {
+                searchPaths.AddRange(imageConfig.Directories);
+            }
+
+            if(SearchPaths?.Split(';')?.Any() ?? false)
+            {
+                searchPaths.AddRange(SearchPaths.Split(';'));
+            }
+
+            var monoandroidKey = imageConfig.ConditionalDirectories.Keys.FirstOrDefault(x => x.Equals("monoandroid", StringComparison.InvariantCultureIgnoreCase));
+            var xamariniOSKey = imageConfig.ConditionalDirectories.Keys.FirstOrDefault(x => x.Equals("xamarin.ios", StringComparison.InvariantCultureIgnoreCase) || x.Equals("xamarinios", StringComparison.InvariantCultureIgnoreCase));
+            var xamarinMacKey = imageConfig.ConditionalDirectories.Keys.FirstOrDefault(x => x.Equals("xamarin.mac", StringComparison.InvariantCultureIgnoreCase) || x.Equals("xamarinmac", StringComparison.InvariantCultureIgnoreCase));
+
+            switch(config.Platform)
+            {
+                case Platform.Android:
+                    if (!string.IsNullOrEmpty(monoandroidKey))
+                        searchPaths.AddRange(imageConfig.ConditionalDirectories[monoandroidKey]);
+                    break;
+                case Platform.iOS:
+                    if (!string.IsNullOrEmpty(xamariniOSKey))
+                        searchPaths.AddRange(imageConfig.ConditionalDirectories[xamariniOSKey]);
+                    break;
+                case Platform.macOS:
+                    if (!string.IsNullOrEmpty(xamarinMacKey))
+                        searchPaths.AddRange(imageConfig.ConditionalDirectories[xamarinMacKey]);
+                    break;
+            }
+
+            // TODO: Return Conditional Directories for current Build Configuration
+
+            return searchPaths;
         }
     }
 }
