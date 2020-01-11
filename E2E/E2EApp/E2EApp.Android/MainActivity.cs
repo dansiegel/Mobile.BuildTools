0d9d6167f571feeab47bc80dc5c462db105f4552
Dan Siegel <me@dansiegel.net>
2020-01-10 21:22:40 -0800

    update for ConfigurationManager

--- E2E/E2EApp/E2EApp.Android/MainActivity.cs
+++ E2E/E2EApp/E2EApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Mobile.BuildTools.Configuration;
 
 namespace E2EApp.Droid
 {
@@ -19,6 +20,7 @@ namespace E2EApp.Droid
 
             base.OnCreate(bundle);
 
+            ConfigurationManager.Init(this);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
