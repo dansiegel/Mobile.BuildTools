9b19a807c56e3c08c4fa7a76776ec2905dfed6c7
sean.alford <sean.alford@icloud.com>
2020-03-13 20:59:30 -0400

    Fixed several issues within the SVG files, and added build batch files.

---
+++ assets/source/build-all.bat
@@ -0,0 +1,17 @@
+cls
+call build-flatblack.bat
+call build-flatblue.bat
+call build-flatgreen.bat
+call build-flatorange.bat
+call build-flatpurple.bat
+call build-flatred.bat
+call build-flatwhite.bat
+call build-flatyellow.bat
+call build-black.bat
+call build-blue.bat
+call build-green.bat
+call build-orange.bat
+call build-purple.bat
+call build-red.bat
+call build-white.bat
+call build-yellow.bat
