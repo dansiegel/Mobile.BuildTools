5654d16a25e66f33c69b8e9833cb50676491afb1
Dan Siegel <me@dansiegel.net>
2021-07-05 16:12:50 -0700

    Add support for GitHub Actions and BitBucket Pipelines

--- src/Mobile.BuildTools.Reference/Utils/CIBuildEnvironmentUtils.cs
+++ src/Mobile.BuildTools.Reference/Utils/CIBuildEnvironmentUtils.cs
@@ -1,34 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace Mobile.BuildTools.Utils
 {
     public static class CIBuildEnvironmentUtils
     {
-        public static bool IsAppCenter => Environment.GetEnvironmentVariables()
+        public static readonly bool IsCI = IsCIInternal();
+
+        public static readonly bool IsAppCenter = Environment.GetEnvironmentVariables()
                                                      .Keys
                                                      .Cast<object>()
                                                      .Any(k => k.ToString() == "APPCENTER_BUILD_ID");
-        public static bool IsAppVeyor => Environment.GetEnvironmentVariables()
+
+        public static readonly bool IsAppVeyor = Environment.GetEnvironmentVariables()
                                                      .Keys
                                                      .Cast<object>()
                                                      .Any(k => k.ToString() == "APPVEYOR_BUILD_NUMBER");
-        public static bool IsTeamCity => Environment.GetEnvironmentVariables()
+
+        public static readonly bool IsTeamCity = Environment.GetEnvironmentVariables()
                                                      .Keys
                                                      .Cast<object>()
                                                      .Any(k => k.ToString() == "TEAMCITY_VERSION");
-        public static bool IsJenkins => Environment.GetEnvironmentVariables()
+
+        public static readonly bool IsJenkins = Environment.GetEnvironmentVariables()
                                                      .Keys
                                                      .Cast<object>()
-                                                     .Any(k => k.ToString() == "BUILD_NUMBER") && !IsTeamCity;
-        public static bool IsVSTS => Environment.GetEnvironmentVariables()
+                                                     .Any(k => k.ToString() == "JENKINS_HOME");
+
+        public static readonly bool IsAzureDevOps = Environment.GetEnvironmentVariables()
                                                      .Keys
                                                      .Cast<object>()
                                                      .Any(k => k.ToString() == "BUILD_BUILDNUMBER");
 
-        public static bool IsBuildHost => IsAppCenter || IsAppVeyor || IsJenkins || IsVSTS;
+        public static readonly bool IsBitBucket = Environment.GetEnvironmentVariables()
+                                                     .Keys
+                                                     .Cast<object>()
+                                                     .Any(k => k.ToString() == "BITBUCKET_BUILD_NUMBER");
+
+        public static readonly bool IsGitHubActions = Environment.GetEnvironmentVariables()
+                                                     .Keys
+                                                     .Cast<object>()
+                                                     .Any(k => k.ToString() == "GITHUB_WORKFLOW");
+
+        public static bool IsBuildHost => 
+            IsCI ||
+            IsAppCenter ||
+            IsAppVeyor ||
+            IsJenkins ||
+            IsAzureDevOps ||
+            IsBitBucket ||
+            IsGitHubActions;
 
         public static string BuildNumber
         {
@@ -42,17 +63,36 @@ namespace Mobile.BuildTools.Utils
                 {
                     return Environment.GetEnvironmentVariable("APPVEYOR_BUILD_NUMBER");
                 }
-                else if(IsJenkins)
+                else if(IsBitBucket)
+                {
+                    return Environment.GetEnvironmentVariable("BITBUCKET_BUILD_NUMBER");
+                }
+                else if(IsJenkins || IsTeamCity)
                 {
                     return Environment.GetEnvironmentVariable("BUILD_NUMBER");
                 }
-                else if(IsVSTS)
+                else if (IsGitHubActions)
+                {
+                    return Environment.GetEnvironmentVariable("GITHUB_RUN_ID");
+                }
+                else if(IsAzureDevOps)
                 {
                     return Environment.GetEnvironmentVariable("BUILD_BUILDNUMBER");
                 }
 
                 return null;
             }
+        }
+
+        private static bool IsCIInternal()
+        {
+            var isCIString = Environment.GetEnvironmentVariable("CI");
+            if (string.IsNullOrEmpty(isCIString))
+                return false;
+            else if (bool.TryParse(isCIString, out var isCI))
+                return isCI;
+
+            return false;
         }
     }
 }
